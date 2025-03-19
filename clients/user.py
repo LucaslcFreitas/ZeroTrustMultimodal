51ce07105c684af7ea64a0997085785665bcf8e6
Lucas Freitas <lucaslcfjf@hotmail.com>
2025-03-19 14:10:11 -0300

    Clientes para docker a corrigir

--- clients/user.py
+++ clients/user.py
@@ -4,32 +4,51 @@ import os
 import json
 import time
 
-HOST = '127.0.0.1'
-PORT = 5000
-
-def startAccess(test):
+def startAccess():
+    print("antes do sleep")
+    time.sleep(5)
+    print("depois do sleep")
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        print("with socket")
+        HOST = os.getenv("IP_ZERO_TRUST")
+        PORT = int(os.getenv("PORT_ZERO_TRUST", 5000))
+
         # Dados dos usuários
         users = {}
 
-        # Arquivo com as instâncias de acesso
-        with open(os.path.dirname(os.path.abspath(__file__)) + "/scenarios/"+test+".json") as file:
-            instance = json.load(file)
-        
-        # Conexão com o servidor Zero Trust (aceitando certificado ssl auto assinado)
-        context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-        context.check_hostname = False
-        context.verify_mode = ssl.CERT_NONE
-        conn = context.wrap_socket(sock, server_hostname=HOST)
+        # Operações de acesso
+        operations_json = os.getenv("OPERATIONS")
+        print("operation_json")
 
-        conn.connect((HOST, PORT))
-        print(f'Conected to {HOST}:{PORT}')
+        if operations_json:
+            operations = json.loads(operations_json)
+        else:
+            print("No operations found")
+            exit()
+        
+        try:
+            print("dados da conexão")
+            # Conexão com o servidor Zero Trust (aceitando certificado ssl auto assinado)
+            context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+            context.check_hostname = False
+            context.verify_mode = ssl.CERT_NONE
+            conn = context.wrap_socket(sock, server_hostname=HOST)
+
+            print("antes da conexão")
+            conn.connect((HOST, PORT))
+            print(f'Conected to {HOST}:{PORT}')
+
+        except Exception as e:
+            print(e)
+            exit()
 
         average_time = 0.0
         access_count = 0
 
-        if instance:
-            for access in instance:
+        if operations:
+            print(len(operations))
+            for access in operations:
+                print(access["TYPE"])
                 match (access['TYPE']):
                     case 'LOGIN':
                         message = getLogin(access['REGISTRY'], access['PASSWORD'], access['IP_ADDRESS'], access['LATITUDE'], access['LONGITUDE'], access['MAC'], access['DFP'], access['OS'], access['VERSION_OS'], access['TIME'])
@@ -50,7 +69,7 @@ def startAccess(test):
                             users[access['REGISTRY']]['PASSWORD'] = access['PASSWORD']
                             users[access['REGISTRY']]['TOKEN'] = data['TOKEN']
                             print(data['RESULT'])
-                        elif users[access['REGISTRY']]['TOKEN'] == None:
+                        elif users[access['REGISTRY']] != None and users[access['REGISTRY']]['TOKEN'] == None:
                             print("Login error")
                             break
                     
@@ -205,47 +224,5 @@ def getUpdatePassword(token, oldPassword, newPassword, ip, latitude, longitude, 
     return message
 
 if __name__ == '__main__':
-    print("SELECIONE O CENÁRIO:")
-    print("1 - Cenário 1 - Acesso Normal")
-    print("2 - Cenário 2 - Roubo de Token - Teste 1")
-    print("3 - Cenário 2 - Roubo de Token - Teste 2")
-    print("4 - Cenário 3 - Roubo de Credenciais")
-    print("5 - Cenário 4 - Ataque de Força Bruta - Sem Sucesso")
-    print("6 - Cenário 4 - Ataque de Força Bruta - Com Sucesso")
-    print("7 - Cenário 5 - Dispositivo Compartilhado")
-    print("8 - Cenário 6 - Acesso fora do Horário - Teste 1")
-    print("9 - Cenário 6 - Acesso fora do Horário - Teste 2")
-    print("10 - Cenário 6 - Acesso fora do Horário - Teste 3")
-    print("11 - Cenário 6 - Acesso fora do Horário - Teste 4")
-    print("12 - SAIR")
-
-    option = input("Opção: ")
-    while option != 13:
-        match option:
-            case "1":
-                startAccess("instance-C1")
-            case "2":
-                startAccess("instance-C2T1")
-            case "3":
-                startAccess("instance-C2T2")
-            case "4":
-                startAccess("instance-C3")
-            case "5":
-                startAccess("instance-C4T1")
-            case "6":
-                startAccess("instance-C4T2")
-            case "7":
-                startAccess("instance-C5")
-            case "8":
-                startAccess("instance-C6T1")
-            case "9":
-                startAccess("instance-C6T2")
-            case "10":
-                startAccess("instance-C6T3")
-            case "11":
-                startAccess("instance-C6T4")
-            case "12":
-                break
-            case _:
-                print("Opção Inválida!")
-        option = input("Opção: ")
+    print("Start")
+    startAccess()
